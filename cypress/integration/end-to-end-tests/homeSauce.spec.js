3cda9ebf91651b90a2c4561716e7cfe2568c9eef
dor-fried <dorifried@gmail.com>
2021-11-30 14:06:33 +0200

    INSERT_NEW_CODE

--- cypress/integration/end-to-end-tests/homeSauce.spec.js
+++ cypress/integration/end-to-end-tests/homeSauce.spec.js
@@ -26,4 +26,24 @@ describe('Demo web tests', () => {
         inventoryPage.elements.errorMessage()
             .should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
     });
+
+    it('should display incorrect username message', function () {
+
+        homeSoucePage.typeUsername('dor123');
+        homeSoucePage.typePassword('secret_sauce');
+        homeSoucePage.clickLogin();
+
+        inventoryPage.elements.errorMessage()
+            .should('have.text', 'Epic sadface: Username and password do not match any user in this service');
+    });
+
+    it('should display incorrect password message', function () {
+
+        homeSoucePage.typeUsername('locked_out_user');
+        homeSoucePage.typePassword('gsgsd');
+        homeSoucePage.clickLogin();
+
+        inventoryPage.elements.errorMessage()
+            .should('have.text', 'Epic sadface: Username and password do not match any user in this service');
+    });
 });

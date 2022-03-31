31d6f50c5318c4d7fa8d01fabaa7368fd3b5e8a8
dor-fried <dorifried@gmail.com>
2022-03-31 11:25:03 +0300

    new_code

--- cypress/pages/inventoryPage.js
+++ cypress/pages/inventoryPage.js
@@ -1,8 +1,12 @@
 class inventoryPage {
 
     elements = {
-        titleSpan: () => cy.get('.title')
-    }
+        titleSpan: () => cy.get('.title'),
+        MainBurgerButton: () => cy.get('.bm-burger-button button'),
+        InventoryItems: () => cy.get('.inventory_list .inventory_item'),
+        CartListItems: () => cy.get('.cart_list .cart_item'),
+        ShoppingCartBadge: () => cy.get('#shopping_cart_container .shopping_cart_badge'),
+    };
 }
 
 module.exports = new inventoryPage();
